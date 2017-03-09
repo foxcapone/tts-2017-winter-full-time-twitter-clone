efb7533d66ee7b821833c71eea9d73b1d044b8b9
Jeffrey Woodard <icikalu@gmail.com>
2017-03-09 09:41:19 -0600

    Set up profile index and show view

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,5 +4,5 @@ class Tweet < ApplicationRecord
   validates :message, presence: true, length: { maximum: 140, too_long: 'A tweet is only 140 characters max.'}
   has_many :tweets
 
-  validates :username, presence: true, uniqueness: true
+
 end
