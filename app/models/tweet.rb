9e361d0acef357d06d7eeccef9f4502b80519b4b
Jeffrey Woodard <icikalu@gmail.com>
2017-03-09 09:03:23 -0600

    Add validation for tweets and user accounts

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,8 @@
 class Tweet < ApplicationRecord
   belongs_to :user
+
+  validates :message, presence: true, length: { maximum: 140, too_long: 'A tweet is only 140 characters max.'}
+  has_many :tweets
+
+  validates :username, presence: true, uniqueness: true
 end
